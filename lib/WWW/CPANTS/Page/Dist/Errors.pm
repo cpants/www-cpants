27093302b440c0c2a00c403f1b1ef21cfbf7abe1
Kenichi Ishigaki <ishigaki@cpan.org>
2013-01-01 23:40:48 +0900

    added dist/errors to fix #8

---
+++ lib/WWW/CPANTS/Page/Dist/Errors.pm
@@ -0,0 +1,51 @@
+package WWW::CPANTS::Page::Dist::Errors;
+
+use strict;
+use warnings;
+use WWW::CPANTS::DB;
+use WWW::CPANTS::Kwalitee;
+use WWW::CPANTS::Utils;
+
+sub load_data {
+  my ($class, $name) = @_;
+
+  my $dist = db_r('Kwalitee')->fetch_distv($name);
+  return unless $dist && $dist->{distv};
+
+  my $errors = db_r('Errors')->fetch_distv_errors($dist->{distv});
+
+  my %data = (
+    dist => $dist,
+    errors => $errors,
+  );
+  return \%data;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+WWW::CPANTS::Page::Dist::Errors
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 load_data
+
+=head1 AUTHOR
+
+Kenichi Ishigaki, E<lt>ishigaki@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Kenichi Ishigaki.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
