dee569ec047fb2bade065ca2a10dfe159245f108
Kenichi Ishigaki <ishigaki@cpan.org>
2020-08-01 09:49:22 +0900

    Remove unused methods in Table::Permissions

--- lib/WWW/CPANTS/DB/Table/Permissions.pm
+++ lib/WWW/CPANTS/DB/Table/Permissions.pm
@@ -13,23 +13,4 @@ sub indices ($self) { (
     [qw/module/],
 ) }
 
-sub select_all_by_author ($self, $pause_id) {
-    my $sql = <<~';';
-    SELECT * FROM permissions
-    WHERE pause_id = ?
-    ;
-    $self->select_all($sql, $pause_id);
-}
-
-sub select_unauthorized_modules_for_author ($self, $pause_id, $modules) {
-    my $sql = <<~';';
-    SELECT module FROM (
-      SELECT module FROM permissions WHERE module IN (:modules) GROUP BY module
-    )
-    WHERE module NOT IN (SELECT module FROM permissions WHERE pause_id = ? GROUP BY module)
-    GROUP BY module
-    ;
-    $self->select_all($sql, $pause_id, [modules => $modules]);
-}
-
 1;
